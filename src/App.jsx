02b4af5e7432072c584af83371d33810e010c9a9
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2026-01-14 11:51:02 +0600

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen">
+      <h1 className="text-center pt-10 text-4xl font-bold text-white">
+        Counter App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
